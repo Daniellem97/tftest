e4622b99477053d3c3fa6fc23b4adde76a2812a9
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-06-07 12:48:00 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -71,6 +71,10 @@ resource "aws_key_pair" "mtc_auth" {
   key_name   = "mtckey2"
   public_key = file("mtckey.pub")
 }
+resource "aws_key_pair" "mtc_auth1" {
+  key_name   = "mtckey2"
+  public_key = file("mtckey.pub")
+}
 
 resource "aws_instance" "dev_node" {
     instance_type = "t2.micro"
