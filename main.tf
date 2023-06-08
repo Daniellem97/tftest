e08f4199abe0afe9788ae6340157257a0ce877c3
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-06-08 09:59:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,13 +43,6 @@ resource "aws_route_table" "mtc_public_rt2" {
   }
 }
 
-resource "aws_route_table" "mtc_public_rt3" {
-  vpc_id = aws_vpc.mtc_vpc.id
-
-  tags = {
-    Name = "dev_public_rt"
-  }
-}
 
 
 
