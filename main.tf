012e5a6549252046a5722c3f5050dc1aa7ab6a97
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-06-11 19:38:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -83,7 +83,18 @@ resource "aws_instance" "dev_node" {
     root_block_device {
         volume_size = 10
     }
-    
+    resource "aws_instance" "dev_nodetest" {
+    instance_type = "t2.micro"
+    ami = data.aws_ami.server_ami.id
+    key_name = aws_key_pair.mtc_auth.id 
+    vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+    subnet_id = aws_subnet.mtc_public_subnet.id
+    user_data = file("userdata.tpl")
+
+    root_block_device {
+        volume_size = 10
+    }
+      
     tags = {
         Name = "dev-node"
     }
