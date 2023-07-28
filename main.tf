a0671094f22faa93affbfe1d835161f8258061ce
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-07-28 15:14:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,29 +7,6 @@ resource "aws_vpc" "mtc_vpc" {
     Name = "dev"
   }
 }
-
-resource "aws_iam_role" "test_role" {
-  name = "test_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": "examplestatement"
-    }
-  ]
-}
-EOF
-
-  tags = {
-    tag-key = "tag-value"
-  }
 }
 
 resource "aws_subnet" "mtc_public_subnet" {
