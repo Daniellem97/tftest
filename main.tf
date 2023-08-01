f668cb3275de1ff30be39227f03d62ab4b654f12
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-08-01 10:59:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,6 @@ resource "aws_subnet" "mtc_public_subnet" {
   vpc_id                  = aws_vpc.mtc_vpc.id
   cidr_block              = "10.123.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-west-1a"
 
   tags = {
     Name = "dev-public"
