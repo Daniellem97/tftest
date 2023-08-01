24a0afe1240bb8d3c59cc8a8f961a77bb816c0d8
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-08-01 13:24:44 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "aws" {
-  region                  = 'us-west-1'
+  region                  = "us-west-2"
   shared_credentials_file = "~/.aws/credentials"
   profile                 = "default"
 }
