19528fa051879b41df29c1ef5c665313ab80917e
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-08-05 16:13:40 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_security_group" "mtc_sg" {
 
 
 resource "aws_instance" "dev_node" {
-    instance_type = "t2.large"
+    instance_type = "t2.micro"
     ami = data.aws_ami.server_ami.id
     key_name = aws_key_pair.mtc_auth.id 
     vpc_security_group_ids = [aws_security_group.mtc_sg.id]
