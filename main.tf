6639eb933ebbad7712f73647209b0e1c8dd86a9a
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-08-19 23:24:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
-resource "aws_iam_user" "lb" {
-  name = "loadbalancer"
-  path = "/system/"
 
-  tags = {
-    tag-key = "tag-value"
-  }
-}
 
 resource "aws_vpc" "mtc_vpc" {
   cidr_block           = "10.123.0.0/16"
