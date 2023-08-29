a5b82ea2efb9657f54b27773e4d7eba70ec46845
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-08-29 20:54:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,16 @@ resource "aws_subnet" "mtc_public_subnet" {
   }
 }
 
+resource "aws_subnet" "mtc_public_subnet2" {
+  vpc_id                  = aws_vpc.mtc_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "dev-public2"
+  }
+}
+
 resource "aws_internet_gateway" "mtc_internet_gateway" {
   vpc_id = aws_vpc.mtc_vpc.id
 
