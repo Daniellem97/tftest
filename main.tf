0f8761b958ed7542255653f65a285ac979ee438e
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-11-13 16:07:19 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,17 +8,6 @@ resource "aws_vpc" "mtc_vpc" {
   }
 }
 
-resource "aws_vpc" "mtc_vpc1" {
-  cidr_block           = "10.123.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    Name = "dev3"
-  }
-}
-
-
-
 resource "aws_subnet" "mtc_public_subnet" {
   vpc_id                  = aws_vpc.mtc_vpc.id
   cidr_block              = "10.123.1.0/24"
