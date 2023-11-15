39516df242d9696f471aaa006d2e497b5f3914ad
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-11-15 15:34:40 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -72,7 +72,20 @@ resource "aws_instance" "dev_node" {
     Name = "ExampleInstance"
     Environment = "Dev"                        
   }
-    
+
+resource "aws_lb" "example" {
+  name               = "example-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [aws_subnet.example.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "example-lb"
+  }
+}
 
   #provisioner "local-exec" {
   #  command = templatefile("${var.host_os}-ssh-config.tpl", {
