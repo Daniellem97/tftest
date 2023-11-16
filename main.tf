041f3079328cdc4a9be221b7fe9557355e4753bd
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-11-16 11:56:27 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -74,9 +74,20 @@ resource "aws_instance" "dev_node" {
   }
 }
 
-module "test" {
-  source  = "spacelift.io/daniellem97/terraform-module-test/test"
-  version = "0.0.1"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.11.0"  # Specify a version that suits your requirements
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
 
 
   #provisioner "local-exec" {
