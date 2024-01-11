3475fb4e608ab59acd49fbf5acaaa5f6f24144ff
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-01-11 11:03:13 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,6 +82,19 @@ resource "aws_instance" "dev_node" {
   }
 }
 
+resource "aws_db_instance" "example" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "user"
+  password             = "password"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
+
   #  command = templatefile("${var.host_os}-ssh-config.tpl", {
   #    hostname = self.public_ip,
   #    user     = "ubuntu",
