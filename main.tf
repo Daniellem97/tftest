48c600b85fda4cbaf9a7d1ab9f673d4ada751b73
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-01-20 18:23:27 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "mtc_public_subnet" {
   cidr_block              = "10.123.1.0/24"
   map_public_ip_on_launch = true
   tags = {
-    Name = "dev-public"
+    Name = "dev-public2"
   }
 }
 
@@ -25,7 +25,7 @@ resource "aws_vpc" "mtc_vpc1" {
   }
 }
 
-resource "aws_vpc" "mtc_vpc2" {
+resource "aws_vpc" "mtc_vpc0" {
   cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
@@ -33,7 +33,6 @@ resource "aws_vpc" "mtc_vpc2" {
     Name = "dev2"
   }
 }
-
 
 
 resource "aws_internet_gateway" "mtc_internet_gateway" {
