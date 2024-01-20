1a4faab4ed5f431d8aec5fc4589122d711871760
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-01-20 18:01:44 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,10 +34,12 @@ resource "aws_route" "default_route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.mtc_internet_gateway.id
 }
+
 resource "aws_route_table_association" "mtc_public_assoc" {
-  subnet_id      = aws_subnet..id
+  subnet_id      = aws_subnet.mtc_public_subnet.id
   route_table_id = aws_route_table.mtc_public_rt.id
 }
+
 resource "aws_security_group" "mtc_sg" {
   name        = "public_sg"
   description = "public security group"
