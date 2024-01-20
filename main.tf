d4d8ae7f57dc5da04cfca15c95597b14a734e933
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-01-20 17:52:02 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "mtc_vpc" {
   }
 }
 
-resource "aws_subnet" "mtc_public_subnet" {
+resource "aws_subnet" "mtc_public_subnet2" {
   vpc_id                  = aws_vpc.mtc_vpc.id
   cidr_block              = "10.123.1.0/24"
   map_public_ip_on_launch = true
@@ -23,12 +23,6 @@ resource "aws_internet_gateway" "mtc_internet_gateway" {
   }
 }
 
-resource "aws_route_table" "mtc_public_rt" {
-  vpc_id = aws_vpc.mtc_vpc.id
-  tags = {
-    Name = "dev_public_rt"
-  }
-}
 resource "aws_route" "default_route" {
   route_table_id         = aws_route_table.mtc_public_rt.id
   destination_cidr_block = "0.0.0.0/0"
