8325d24333aea3bdcb0bec5b0264aad678b349c0
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-01-30 10:08:58 +0000

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -7,13 +7,3 @@ data "aws_ami" "server_ami" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
 }
-
-data "aws_ami" "server_ami2" {
-  most_recent = true
-  owners      = ["099720109477"]
-
-  filter {
-    name   = "name2"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-  }
-}
