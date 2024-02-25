0644e7d60455d6e6b3f12913ce9b58d9716f566d
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-02-25 20:16:39 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,16 @@ resource "aws_vpc" "mtc_vpc" {
   }
 }
 
+resource "aws_subnet" "mtc_public_subnet1" {
+  vpc_id                  = aws_vpc.mtc_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "dev-public"
+  }
+}
+
+
 resource "aws_subnet" "mtc_public_subnet" {
   vpc_id                  = aws_vpc.mtc_vpc.id
   cidr_block              = "10.123.1.0/24"
@@ -15,6 +25,18 @@ resource "aws_subnet" "mtc_public_subnet" {
     Name = "dev-public"
   }
 }
+
+
+
+resource "aws_subnet" "mtc_public_subnet" {
+  vpc_id                  = aws_vpc.mtc_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "dev-public"
+  }
+}
+
 
 
 resource "aws_internet_gateway" "mtc_internet_gateway" {
