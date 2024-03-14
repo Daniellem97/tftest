6e7bfaa50bf1ccc3712614ce6d86319836ab0b19
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-14 15:37:33 +0000

    Update outputs.tf

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,8 +1,7 @@
 output "iam_role_arn" {
-  value       = aws_iam_role.example_role.arn
+  value       = aws_iam_role.testing_importiam.arn
   description = "The ARN of the IAM role"
 }
-
 output "iam_policy_name" {
   value       = aws_iam_policy_attachment.admin_access.name
   description = "The name of the IAM policy attached to the role"
