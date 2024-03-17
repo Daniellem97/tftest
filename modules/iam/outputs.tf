27fe86193dfa0952dbb6eb10b492b8af0a65ccdd
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-17 18:57:40 +0000

    Delete modules directory

--- modules/iam/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "iam_role_arn" {
-  value       = aws_iam_role.testingimportiam.arn
-  description = "The ARN of the IAM role"
-}
-output "iam_policy_name" {
-  value       = aws_iam_policy_attachment.admin_access.name
-  description = "The name of the IAM policy attached to the role"
-}
