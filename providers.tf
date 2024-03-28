871d2f27837878ef17bc4cb635799299732b97d8
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-28 09:27:14 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.71.0"  # Changed version constraint format
+      version = "~> 3.71.0"  # This specifies to use a version in the 3.x range, but at least 3.71.0 or newer patch versions
     }
   }
-}
-provider "aws" {
+  required_version = "~> 0.12.29" 
 }
 
-required_version = "~> 0.12.29"
+provider "aws" {
 }
