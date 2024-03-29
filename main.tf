ed7e810e0311bd7ef076a0d1a09ab13705ff7bfd
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-29 13:24:04 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,20 +63,6 @@ resource "aws_security_group" "mtc_sg" {
   public_key = file("mtckey.pub")
 }
 
-resource "aws_instance" "dev_node" {
-    instance_type = "t2.micro"
-    ami = data.aws_ami.server_ami.id
-    key_name = aws_key_pair.mtc_auth.id 
-    vpc_security_group_ids = [aws_security_group.mtc_sg.id]
-    subnet_id = aws_subnet.mtc_public_subnet.id
-    user_data = file("userdata.tpl")
-    tags = {
-    Name = "ExampleInstance"
-    Environment = "Dev"                        
-  }
-}
-
-
 
   #  command = templatefile("${var.host_os}-ssh-config.tpl", {
   #    hostname = self.public_ip,
