801c4e29f44057333e9b2153f1db0c7e84d7eeb9
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-04-04 11:43:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "mtc_vpc" {
   }
 }
 
-resource "aws_subnet" "mtc_public_subnet" {
+resource "aws_subnt" "mtc_public_subnet" {
   vpc_id                  = aws_vpc.mtc_vpc.id
   cidr_block              = "10.123.1.0/24"
   map_public_ip_on_launch = true
