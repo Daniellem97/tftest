4c47d6f08ee6c3ea55525536505903b8fcde913e
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-05-13 12:29:59 +0100

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -7,3 +7,16 @@ data "aws_ami" "server_ami" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
 }
+
+data "terraform_remote_state" "demo-stack-state" {
+  backend = "remote"
+
+  config = {
+    hostname     = "spacelift.io"
+    organization = "daniellem97"
+
+    workspaces = {
+      name = "demo-stack"
+    }
+  }
+}
