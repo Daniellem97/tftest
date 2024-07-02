8687af2f3d1f2950a8917b59dccc177b7daeae0c
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-07-02 17:04:50 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,3 @@ output "dev_ip2" {
     value     = "test"
     sensitive = true
 }
-
-resource "random_string" "example" {
-  length  = 8
-}
-
-output "trigger" {
-  value     = random_string.example.result
-}
