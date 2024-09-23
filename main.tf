498ee86b6a24e931ecf360a634bb90f94f90151c
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-09-23 14:33:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,9 +11,10 @@ resource "aws_vpc" "mtc_vpc4" {
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
-    Name = "dev21"
+    Name = "dev2"
   }
 }
+
 resource "aws_route" "default_route2" {
   route_table_id         = aws_route_table.mtc_public_rt.id
   destination_cidr_block = "0.0.0.0/0"
