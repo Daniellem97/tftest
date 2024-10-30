71c31cdcc6d9ea92cfa4d2cdc160e55a07283e8e
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-10-30 18:47:45 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,14 +7,9 @@ resource "aws_vpc" "mtc_vpc" {
   }
 }
 
-resource "aws_vpc" "mtc_vpc6" {
-  cidr_block           = "10.123.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    Name = "dev213"
+removed {
+  from = aws_vpc.mtc_vpc6
   }
-}
 
 resource "aws_route" "default_route2" {
   route_table_id         = aws_route_table.mtc_public_rt.id
