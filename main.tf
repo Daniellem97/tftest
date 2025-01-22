713c927987e131e8334a6e5b1eb34aea7fb957f0
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-01-22 13:54:17 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,14 +15,6 @@ resource "aws_vpc" "mtc_vpc" {
   }
 }
 
-resource "aws_vpc" "mtc_vpc80" {
-  cidr_block           = "10.123.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    Name = "dev213"
-  }
-}
 resource "aws_vpc" "mtc_vpc1" {
   cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true
