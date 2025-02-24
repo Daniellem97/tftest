0f80f2d94fe3a0d9d96dc25e9deac9582331f519
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-02-24 09:32:27 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,31 +7,6 @@ resource "aws_vpc" "mtc_vpc" {
   }
 }
 
-resource "aws_vpc" "mtc_vpc1" {
-  cidr_block           = "10.123.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    Name = "dev213"
-  }
-}
-resource "aws_vpc" "mtc_vpc2" {
-  cidr_block           = "10.123.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    Name = "dev213"
-  }
-}
-resource "aws_vpc" "mtc_vpc3" {
-  cidr_block           = "10.123.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    Name = "dev213"
-  }
-}
-
 resource "aws_route" "default_route2" {
   route_table_id         = aws_route_table.mtc_public_rt.id
   destination_cidr_block = "0.0.0.0/0"
