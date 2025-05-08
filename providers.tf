89e8342a32abfed5e6218adc1d8e9e6cd71d300e
Danielle <daniellem97@Danielles-MacBook-Air.local>
2025-05-08 12:02:26 +0100

    second commit

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.71.0"  # This specifies to use a version in the 3.x range, but at least 3.71.0 or newer patch versions
+      version = "~> 3.71.1"  # This specifies to use a version in the 3.x range, but at least 3.71.0 or newer patch versions
     }
   }
 }
